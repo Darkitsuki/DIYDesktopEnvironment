36d4b6fd65abb132ae643541afd42078dfd0548d
Darkitsuki <nguyenhoahung0817@gmail.com>
2026-08-28 13:17:52 +0700

    update config

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -251,10 +251,10 @@ hl.config({
 
         follow_mouse = 1,
 
-        sensitivity = -0.6, -- -1.0 - 1.0, 0 means no modification
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification
 
         touchpad = {
-            natural_scroll = false,
+            natural_scroll = true,
             --tap-to-click = true,
             --disable_while_typing = true,
         },
